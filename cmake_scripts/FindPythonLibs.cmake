96aa7b1a94939720977b241bf76c808e1de9d470
Gabriel <gasevi@Gabriels-iMac.local>
2017-07-28 00:47:09 -0400

    * Adds Python 3 support on Mac OS X.

--- cmake_scripts/FindPythonLibs.cmake
+++ cmake_scripts/FindPythonLibs.cmake
@@ -8,21 +8,46 @@ find_program( ${PACKAGE_NAME} python${CMAKE_PYTHON_VERSION}
 mark_as_advanced( ${PACKAGE_NAME} ) 
 
 # Header path
+set( ${PACKAGE_NAME}_HEADER_PATHS ${${PACKAGE_NAME}_HEADER_PATHS} /usr/include/python${CMAKE_PYTHON_VERSION} )
+set( ${PACKAGE_NAME}_HEADER_PATHS ${${PACKAGE_NAME}_HEADER_PATHS} /usr/local/include/python${CMAKE_PYTHON_VERSION} )
+if( APPLE )
+
+   execute_process( COMMAND python${CMAKE_PYTHON_VERSION} -V
+                    OUTPUT_VARIABLE PKG_VERSION
+                    ERROR_VARIABLE PKG_VERSION
+                    OUTPUT_STRIP_TRAILING_WHITESPACE )
+   string( REPLACE "Python " "" PKG_VERSION ${PKG_VERSION} )
+   string( REPLACE "." ";" VERSION_LIST ${PKG_VERSION} )
+   list( GET VERSION_LIST 0 PKG_VERSION_MAJ )
+   list( GET VERSION_LIST 1 PKG_VERSION_MIN )
+   list( GET VERSION_LIST 2 PKG_VERSION_REL )
+
+   set( ${PACKAGE_NAME}_HEADER_PATHS ${${PACKAGE_NAME}_HEADER_PATHS} /usr/local/Cellar/python${PKG_VERSION_MAJ}/${PKG_VERSION}/Frameworks/Python.framework/Versions/${PKG_VERSION_MAJ}.${PKG_VERSION_MIN}/include/python${PKG_VERSION_MAJ}.${PKG_VERSION_MIN}m )
+   set( ${PACKAGE_NAME}_HEADER_PATHS ${${PACKAGE_NAME}_HEADER_PATHS} /usr/local/Cellar/python${PKG_VERSION_MAJ}/${PKG_VERSION}/Frameworks/Python.framework/Versions/${PKG_VERSION_MAJ}.${PKG_VERSION_MIN}/include/python${PKG_VERSION_MAJ}.${PKG_VERSION_MIN} )
+
+endif( APPLE )
+
 set( ${PACKAGE_NAME}_INCLUDE_DIR ${PACKAGE_NAME}_INCLUDE_DIR-NOTFOUND )
 find_path( ${PACKAGE_NAME}_INCLUDE_DIR NAMES Python.h
-                                       PATHS /usr/include/python${CMAKE_PYTHON_VERSION}
-                                             /usr/local/include/python${CMAKE_PYTHON_VERSION}
+                                       PATHS ${${PACKAGE_NAME}_HEADER_PATHS}
+                                       NO_DEFAULT_PATH
                                        DOC   "Directory containing ${PACKAGE_NAME}_HEADERS for ${PACKAGE_NAME} library" )
 mark_as_advanced( ${PACKAGE_NAME}_INCLUDE_DIR )
 
 # Library path
+set( ${PACKAGE_NAME}_LIBRARY_PATHS ${${PACKAGE_NAME}_LIBRARY_PATHS} /usr/lib )
+set( ${PACKAGE_NAME}_LIBRARY_PATHS ${${PACKAGE_NAME}_LIBRARY_PATHS} /usr/local/lib )
+set( ${PACKAGE_NAME}_LIBRARY_PATHS ${${PACKAGE_NAME}_LIBRARY_PATHS} /usr/lib/${CMAKE_SYSTEM_PROCESSOR}-linux-gnu )
+
+if( APPLE )
+
+   set( ${PACKAGE_NAME}_LIBRARY_PATHS ${${PACKAGE_NAME}_LIBRARY_PATHS} /usr/local/Cellar/python${PKG_VERSION_MAJ}/${PKG_VERSION}/Frameworks/Python.framework/Versions/${PKG_VERSION_MAJ}.${PKG_VERSION_MIN}/lib )
+
+endif( APPLE )
+
 set( ${PACKAGE_NAME}_LIBRARY ${PACKAGE_NAME}_LIBRARY-NOTFOUND )
 find_library( ${PACKAGE_NAME}_LIBRARY NAMES python${CMAKE_PYTHON_VERSION}
-                                            python${CMAKE_PYTHON_VERSION}m
-                                      PATHS /usr/lib
-                                            /usr/local/lib
-                                            /usr/lib/i386-linux-gnu
-                                            /usr/lib/x86_64-linux-gnu
+                                      PATHS ${${PACKAGE_NAME}_LIBRARY_PATHS}
                                       DOC   "${PACKAGE_NAME} library file" )
 mark_as_advanced( ${PACKAGE_NAME}_LIBRARY )
 
