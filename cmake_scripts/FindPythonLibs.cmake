6f7ef9baf105f1e2fda715f837a0d5455c45a4db
Gabriel <gasevi@gmail.com>
2017-10-16 14:31:42 -0300

    * Some changes to generate a PyPI compliant wheel package.

--- cmake_scripts/FindPythonLibs.cmake
+++ cmake_scripts/FindPythonLibs.cmake
@@ -1,15 +1,34 @@
+
+#
+# Preamble
+#
+
 set( PACKAGE_NAME PYTHON )
 
+string( REGEX REPLACE "\\." "" MANYLNX_SUBDIR "${CMAKE_PYTHON_VERSION}" )
+set( MANYLNX_SUBDIR "cp${MANYLNX_SUBDIR}-cp${MANYLNX_SUBDIR}m" )
+
+#
 # Executable file
+#
+
+set( ${PACKAGE_NAME}_EXE_PATHS ${${PACKAGE_NAME}_EXE_PATHS} /opt/python/${MANYLNX_SUBDIR}/bin )
+set( ${PACKAGE_NAME}_EXE_PATHS ${${PACKAGE_NAME}_EXE_PATHS} /usr/bin )
+set( ${PACKAGE_NAME}_EXE_PATHS ${${PACKAGE_NAME}_EXE_PATHS} /usr/local/bin )
+
 set( ${PACKAGE_NAME} ${PACKAGE_NAME}-NOTFOUND )
 find_program( ${PACKAGE_NAME} python${CMAKE_PYTHON_VERSION}
-                              /usr/bin
-                              /usr/local/bin )
+                              ${${PACKAGE_NAME}_EXE_PATHS} )
 mark_as_advanced( ${PACKAGE_NAME} ) 
 
+#
 # Header path
+#
+
 set( ${PACKAGE_NAME}_HEADER_PATHS ${${PACKAGE_NAME}_HEADER_PATHS} /usr/include/python${CMAKE_PYTHON_VERSION} )
 set( ${PACKAGE_NAME}_HEADER_PATHS ${${PACKAGE_NAME}_HEADER_PATHS} /usr/local/include/python${CMAKE_PYTHON_VERSION} )
+set( ${PACKAGE_NAME}_HEADER_PATHS ${${PACKAGE_NAME}_HEADER_PATHS} /opt/python/${MANYLNX_SUBDIR}/include/python${CMAKE_PYTHON_VERSION} )
+set( ${PACKAGE_NAME}_HEADER_PATHS ${${PACKAGE_NAME}_HEADER_PATHS} /opt/python/${MANYLNX_SUBDIR}/include/python${CMAKE_PYTHON_VERSION}m )
 if( APPLE )
 
    execute_process( COMMAND python${CMAKE_PYTHON_VERSION} -V
@@ -27,6 +46,10 @@ if( APPLE )
 
 endif( APPLE )
 
+if( WIN32 )
+   set( ${PACKAGE_NAME}_HEADER_PATHS ${${PACKAGE_NAME}_HEADER_PATHS} "c:/Archivos de programa/Anaconda2/include" )
+endif( WIN32 )
+
 set( ${PACKAGE_NAME}_INCLUDE_DIR ${PACKAGE_NAME}_INCLUDE_DIR-NOTFOUND )
 find_path( ${PACKAGE_NAME}_INCLUDE_DIR NAMES Python.h
                                        PATHS ${${PACKAGE_NAME}_HEADER_PATHS}
@@ -34,10 +57,14 @@ find_path( ${PACKAGE_NAME}_INCLUDE_DIR NAMES Python.h
                                        DOC   "Directory containing ${PACKAGE_NAME}_HEADERS for ${PACKAGE_NAME} library" )
 mark_as_advanced( ${PACKAGE_NAME}_INCLUDE_DIR )
 
+#
 # Library path
+#
+
 set( ${PACKAGE_NAME}_LIBRARY_PATHS ${${PACKAGE_NAME}_LIBRARY_PATHS} /usr/lib )
 set( ${PACKAGE_NAME}_LIBRARY_PATHS ${${PACKAGE_NAME}_LIBRARY_PATHS} /usr/local/lib )
 set( ${PACKAGE_NAME}_LIBRARY_PATHS ${${PACKAGE_NAME}_LIBRARY_PATHS} /usr/lib/${CMAKE_SYSTEM_PROCESSOR}-linux-gnu )
+set( ${PACKAGE_NAME}_LIBRARY_PATHS ${${PACKAGE_NAME}_LIBRARY_PATHS} /opt/python/${MANYLNX_SUBDIR}/lib/python${PKG_VERSION_MAJ} )
 
 if( APPLE )
 
@@ -51,7 +78,10 @@ find_library( ${PACKAGE_NAME}_LIBRARY NAMES python${CMAKE_PYTHON_VERSION}
                                       DOC   "${PACKAGE_NAME} library file" )
 mark_as_advanced( ${PACKAGE_NAME}_LIBRARY )
 
+#
 # Debug library path
+#
+
 set( ${PACKAGE_NAME}_LIBRARY_DEBUG ${PACKAGE_NAME}_LIBRARY_DEBUG-NOTFOUND )
 mark_as_advanced( ${PACKAGE_NAME}_LIBRARY_DEBUG )
 
