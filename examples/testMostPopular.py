7ced5c38698d96b2c98505db41469d37656688f3
Gabriel <gasevi@gmail.com>
2019-10-25 18:45:58 -0300

    * Add item and rating column parameters to this example script.

--- examples/testMostPopular.py
+++ examples/testMostPopular.py
@@ -26,6 +26,8 @@ if __name__ == '__main__':
                                                dlmchar = b'\t',
                                                header = False,
                                                usercol = 0,
+                                               itemcol = 1,
+                                               ratingcol = 2,
                                                topn = 10,
                                                output_file = 'ranking.json',
                                                includeRated = False )
